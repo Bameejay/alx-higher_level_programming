df232a3be0afbb22385a184f0b8ef8f0d8bb1417
Bameejay <johnsonkayode01@gmail.com>
2022-06-28 20:12:32 +0000

    python everything is object done

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def magic_string(H=[]):
-    H += ["Holberton"]
-    return (", ".join(H))
+"""Defines a locked class."""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
