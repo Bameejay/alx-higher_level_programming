f76265b8983c6bd4def204aa635c3f8fe220060c
Bameejay <johnsonkayode01@gmail.com>
2022-09-05 16:52:07 -0700

    prinout multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,3 @@
-let cProg = 'C is fun'
-let pythonProg = 'Python is cool'
-let jsProg = 'JavaScript is amazing'
-console.log(cProg)
-console.log(pythonProg)
-console.log(jsProg)
+console.log("C is fun")
+console.log("Python is cool")
+console.log("JavaScript is amazing")
