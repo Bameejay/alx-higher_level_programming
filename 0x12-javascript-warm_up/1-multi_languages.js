008f8de39f83e8f7ff4ee0492ac897e088941e36
Bameejay <johnsonkayode01@gmail.com>
2022-09-05 16:49:37 -0700

    printout multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,6 @@
-let cProg = "C is fun"
-let pythonProg = "Python is cool"
-let jsProg = "JavaScript is amazing"
+let cProg = 'C is fun'
+let pythonProg = 'Python is cool'
+let jsProg = 'JavaScript is amazing'
 console.log(cProg)
 console.log(pythonProg)
 console.log(jsProg)
-
